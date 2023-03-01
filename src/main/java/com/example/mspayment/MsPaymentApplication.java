1026f4838b671d31cb5635748480b3b20e0445dc
Orkhan93 <dokta228@mail.ru>
2023-03-01 12:30:40 +0400

    Added client,request and response layer

---
+++ src/main/java/com/example/mspayment/MsPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.mspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPaymentApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/mspayment/response/PaymentResponse.java
@@ -0,0 +1,22 @@
+package com.example.mspayment.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentResponse {
+
+    private Long id;
+    private BigDecimal amount;
+    private String description;
+    private LocalDateTime responseAt;
+
+}
